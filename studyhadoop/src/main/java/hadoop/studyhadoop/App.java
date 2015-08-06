d43ca88d2585bf471d0aa66936659d2391c104e8
huangzhyu <hzywwj@gmail.com>
2015-08-06 16:20:20 +0800

    add hadoop maven project

---
+++ studyhadoop/src/main/java/hadoop/studyhadoop/App.java
@@ -0,0 +1,13 @@
+package hadoop.studyhadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
