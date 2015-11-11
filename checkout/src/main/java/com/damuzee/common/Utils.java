bfbf9f18f1263c5b04f7db7ba002cb7ea6b5de9e
huangzhaoyu <hzywwj@gmail.com>
2015-11-12 00:45:37 +0800

    add raw type to avoid IDE warming

--- checkout/src/main/java/com/damuzee/common/Utils.java
+++ checkout/src/main/java/com/damuzee/common/Utils.java
@@ -4,11 +4,10 @@ import java.sql.Timestamp;
 import java.util.Map;
 
 public class Utils {
-	@SuppressWarnings("rawtypes")
 	public static <T> boolean isNull(T t){
-		if(t instanceof Map){
+		if(t instanceof Map<?,?>){
 			Map<?,?> map =(Map<?, ?>) t;
-			return map==null || ((Map) t).isEmpty();
+			return map==null || ((Map<?,?>) t).isEmpty();
 		}else if(t instanceof String){
 			String str = (String) t;
 			return str==null || str.trim().equals("");
